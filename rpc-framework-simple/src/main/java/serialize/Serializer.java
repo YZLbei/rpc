9f18da8e59aecc1c835991af937f9bd2150118d7
YZLBei <yuzhenlongbei@outlook.com>
2022-08-12 21:03:48 +0800

    修改序列化接口

--- rpc-framework-simple/src/main/java/serialize/Serializer.java
+++ rpc-framework-simple/src/main/java/serialize/Serializer.java
@@ -1,6 +1,7 @@
 package serialize;
 
 /**
+ * 序列化接口，所有序列化类都要实现这个接口
  * @Auther: YuZhenLong
  * @Date: 2022/6/15 16:13
  * @Description: 序列化接口类
@@ -8,17 +9,20 @@ package serialize;
 public interface Serializer {
     /**
      * 序列化
-     * 
+     *
      * @param obj 要序列化的对象
-     * @return 转化为字节数组
+     * @return 字节数组
      */
-    byte[]serialize(Object obj);
+    byte[] serialize(Object obj);
 
     /**
      * 反序列化
-     * @param clazz 类
-     * @param <T>
+     *
+     * @param bytes 序列化后的字节数组
+     * @param clazz 目标类
+     * @param <T>   类的类型。举个例子,  {@code String.class} 的类型是 {@code Class<String>}.
+     *              如果不知道类的类型的话，使用 {@code Class<?>}
      * @return 反序列化的对象
      */
-    <T> T deserialize(byte[] bytes,Class<T>clazz);
+    <T> T deserialize(byte[] bytes, Class<T> clazz);
 }
