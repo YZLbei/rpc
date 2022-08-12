c594328b4b074f73a95d57dd009604918668d6c7
YZLBei <yuzhenlongbei@outlook.com>
2022-08-12 21:36:42 +0800

    测试

---
+++ rpc-framework-simple/src/test/java/DemoRpcService.java
@@ -0,0 +1,3 @@
+public interface DemoRpcService {
+    String hello();
+}
---
+++ rpc-framework-simple/src/test/java/serialize/kyro/KryoSerializerTest.java
@@ -0,0 +1,30 @@
+package serialize.kyro;
+
+import remoting.dto.RpcRequest;
+import org.junit.jupiter.api.Test;
+import serialize.kryo.KryoSerializer;
+
+import java.util.UUID;
+
+import static org.junit.Assert.assertEquals;
+
+class KryoSerializerTest {
+
+    @Test
+    void kryoSerializerTest() {
+        RpcRequest target = RpcRequest.builder().methodName("hello")
+                .parameters(new Object[]{"sayhelooloo", "sayhelooloosayhelooloo"})
+                .interfaceName("github.javaguide.HelloService")
+                .paramTypes(new Class<?>[]{String.class, String.class})
+                .requestId(UUID.randomUUID().toString())
+                .group("group1")
+                .version("version1")
+                .build();
+        KryoSerializer kryoSerializer = new KryoSerializer();
+        byte[] bytes = kryoSerializer.serialize(target);
+        RpcRequest actual = kryoSerializer.deserialize(bytes, RpcRequest.class);
+        assertEquals(target.getGroup(), actual.getGroup());
+        assertEquals(target.getVersion(), actual.getVersion());
+        assertEquals(target.getRequestId(), actual.getRequestId());
+    }
+}
