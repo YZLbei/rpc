9157266c60c8ea611a02177365e5d4fbb3e96efb
YZLBei <yuzhenlongbei@outlook.com>
2022-08-12 21:01:40 +0800

    修改netty

--- rpc-framework-simple/src/main/java/remoting/transport/netty/NettyClientHandler.java
+++ rpc-framework-simple/src/main/java/remoting/transport/netty/NettyClientHandler.java
@@ -1,45 +1,45 @@
-package remoting.transport.netty;
-
-import dto.RpcResponse;
-import io.netty.channel.ChannelHandlerContext;
-import io.netty.channel.ChannelInboundHandlerAdapter;
-import io.netty.util.AttributeKey;
-import io.netty.util.ReferenceCountUtil;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-/**
- * @Auther: YuZhenLong
- * @Date: 2022/6/15 18:55
- * @Description: 处理服务器返回的RpcResponse
- */
-public class NettyClientHandler extends ChannelInboundHandlerAdapter {
-    private static final Logger logger = LoggerFactory.getLogger(NettyClientHandler.class);
-    @Override
-    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
-        try {
-            //服务器返回的对象
-            RpcResponse rpcResponse = (RpcResponse) msg;
-            logger.info(String.format("client receive msg: %s", rpcResponse));
-            // TODO: 2022/6/15 不懂
-            // 声明一个 AttributeKey 对象
-            AttributeKey<RpcResponse> key = AttributeKey.valueOf("rpcResponse");
-            // 将服务端的返回结果保存到 AttributeMap 上，AttributeMap 可以看作是一个Channel的共享数据源
-            // AttributeMap的key是AttributeKey，value是Attribute
-            ctx.channel().attr(key).set(rpcResponse);
-            ctx.channel().close();
-            
-        }finally {
-            // TODO: 2022/6/15 啥东西 
-            ReferenceCountUtil.release(msg);
-        }
-        
-    }
-
-    @Override
-    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
-        logger.error("client catch exception");
-        cause.printStackTrace();
-        ctx.close();
-    }
-}
+//package remoting.transport.netty;
+//
+//import dto.RpcResponse;
+//import io.netty.channel.ChannelHandlerContext;
+//import io.netty.channel.ChannelInboundHandlerAdapter;
+//import io.netty.util.AttributeKey;
+//import io.netty.util.ReferenceCountUtil;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//
+///**
+// * @Auther: YuZhenLong
+// * @Date: 2022/6/15 18:55
+// * @Description: 处理服务器返回的RpcResponse
+// */
+//public class NettyClientHandler extends ChannelInboundHandlerAdapter {
+//    private static final Logger logger = LoggerFactory.getLogger(NettyClientHandler.class);
+//    @Override
+//    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+//        try {
+//            //服务器返回的对象
+//            RpcResponse rpcResponse = (RpcResponse) msg;
+//            logger.info(String.format("client receive msg: %s", rpcResponse));
+//            // TODO: 2022/6/15 不懂
+//            // 声明一个 AttributeKey 对象
+//            AttributeKey<RpcResponse> key = AttributeKey.valueOf("rpcResponse");
+//            // 将服务端的返回结果保存到 AttributeMap 上，AttributeMap 可以看作是一个Channel的共享数据源
+//            // AttributeMap的key是AttributeKey，value是Attribute
+//            ctx.channel().attr(key).set(rpcResponse);
+//            ctx.channel().close();
+//            
+//        }finally {
+//            // TODO: 2022/6/15 啥东西 
+//            ReferenceCountUtil.release(msg);
+//        }
+//        
+//    }
+//
+//    @Override
+//    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+//        logger.error("client catch exception");
+//        cause.printStackTrace();
+//        ctx.close();
+//    }
+//}
--- rpc-framework-simple/src/main/java/remoting/transport/netty/NettyKryoDecoder.java
+++ rpc-framework-simple/src/main/java/remoting/transport/netty/NettyKryoDecoder.java
@@ -1,56 +1,56 @@
-package remoting.transport.netty;
-
-import io.netty.buffer.ByteBuf;
-import io.netty.channel.ChannelHandlerContext;
-import io.netty.handler.codec.ByteToMessageDecoder;
-import lombok.AllArgsConstructor;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import serialize.Serializer;
-
-import java.util.List;
-
-/**
- * @Auther: YuZhenLong
- * @Date: 2022/6/15 18:22
- * @Description: kryo解码器
- */
-@AllArgsConstructor
-public class NettyKryoDecoder extends ByteToMessageDecoder {
-    private final static Logger logger = LoggerFactory.getLogger(NettyKryoDecoder.class);
-    private Serializer serializer;
-    private Class<?> genericClass;
-    /**
-     * Netty传输的消息长度也就是对象序列化后对应的字节数组的大小，存储在 ByteBuf 头部
-     * 即Bytebuf前四个字节为body的长度
-     */
-    private static final int BODY_LENGTH = 4;
-    
-    @Override
-    protected void decode(ChannelHandlerContext ctx, ByteBuf in, List<Object> out) throws Exception {
-        //1.byteBuf中写入的消息长度所占的字节数已经是4了，所以 byteBuf 的可读字节必须大于 4，
-        if (in.readableBytes()>=BODY_LENGTH){
-            //2.标记readIndex的位置，以便后面重置readIndex 的时候使用
-            in.markReaderIndex();
-            //3.内容的长度
-            int length = in.readInt();
-            //4.遇到不合理的情况直接 return
-            if (length < 0 || in.readableBytes() < 0) {
-                return;
-            }
-            //5.消息不完整
-            // TODO: 2022/6/15 可以用LengthFieldBasedFrameDecoder
-            if (in.readableBytes()<length){
-                in.resetReaderIndex();
-                return;
-            }
-            byte[]body  = new byte[length];
-            //将ByteBuf中的内容读到body中
-            in.readBytes(body);
-            Object obj = serializer.deserialize(body, genericClass);
-            out.add(obj);
-
-
-        }
-    }
-}
+//package remoting.transport.netty;
+//
+//import io.netty.buffer.ByteBuf;
+//import io.netty.channel.ChannelHandlerContext;
+//import io.netty.handler.codec.ByteToMessageDecoder;
+//import lombok.AllArgsConstructor;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//import serialize.Serializer;
+//
+//import java.util.List;
+//
+///**
+// * @Auther: YuZhenLong
+// * @Date: 2022/6/15 18:22
+// * @Description: kryo解码器
+// */
+//@AllArgsConstructor
+//public class NettyKryoDecoder extends ByteToMessageDecoder {
+//    private final static Logger logger = LoggerFactory.getLogger(NettyKryoDecoder.class);
+//    private Serializer serializer;
+//    private Class<?> genericClass;
+//    /**
+//     * Netty传输的消息长度也就是对象序列化后对应的字节数组的大小，存储在 ByteBuf 头部
+//     * 即Bytebuf前四个字节为body的长度
+//     */
+//    private static final int BODY_LENGTH = 4;
+//    
+//    @Override
+//    protected void decode(ChannelHandlerContext ctx, ByteBuf in, List<Object> out) throws Exception {
+//        //1.byteBuf中写入的消息长度所占的字节数已经是4了，所以 byteBuf 的可读字节必须大于 4，
+//        if (in.readableBytes()>=BODY_LENGTH){
+//            //2.标记readIndex的位置，以便后面重置readIndex 的时候使用
+//            in.markReaderIndex();
+//            //3.内容的长度
+//            int length = in.readInt();
+//            //4.遇到不合理的情况直接 return
+//            if (length < 0 || in.readableBytes() < 0) {
+//                return;
+//            }
+//            //5.消息不完整
+//            // TODO: 2022/6/15 可以用LengthFieldBasedFrameDecoder
+//            if (in.readableBytes()<length){
+//                in.resetReaderIndex();
+//                return;
+//            }
+//            byte[]body  = new byte[length];
+//            //将ByteBuf中的内容读到body中
+//            in.readBytes(body);
+//            Object obj = serializer.deserialize(body, genericClass);
+//            out.add(obj);
+//
+//
+//        }
+//    }
+//}
