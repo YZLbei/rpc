6cc7cd69011c879795a068e89975fd15016d374a
YZLBei <yuzhenlongbei@outlook.com>
2022-08-12 20:07:32 +0800

    修改dto

--- rpc-framework-simple/src/main/java/remoting/dto/RpcRequest.java
+++ rpc-framework-simple/src/main/java/remoting/dto/RpcRequest.java
@@ -1,19 +1,27 @@
-package dto;
+package remoting.dto;
 
-import lombok.Builder;
-import lombok.Data;
+import lombok.*;
 
 import java.io.Serializable;
 
 /**
  * 网络传输的实体类，客户端代理服务器将该实体类传输给客户端，包含要调用方法的各种信息
  */
-@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
 @Builder
+@ToString
 public class RpcRequest implements Serializable {
     private static final long serialVersionUID = 1905122041950251207L;
     private String interfaceName;
     private String methodName;
     private Object[] parameters;
     private Class<?>[]paramTypes;
+    private String requestId;
+    private String version;
+    private String group;
+    public String getRpcServiceName() {
+        return this.getInterfaceName() + this.getGroup() + this.getVersion();
+    }
 }
